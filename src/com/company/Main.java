bd5daa9b5c2887aa77e94ef6f9ca1705a5a3187c
k1cboxcer <daniyar.akhmedovv@gmail.com>
2022-09-02 18:17:03 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(20000);
+        while (true){
+            System.out.println("Ваш баланс: " + bankAccount.getAmount());
+            try {
+                bankAccount.withDraw(6000);
+            }catch (LimitException e){
+                System.out.println(e.getMessage() + "Была вызято: " + e.getRemainingAmount());
+                try {
+                    bankAccount.withDraw((int) e.getRemainingAmount());
+                    System.out.println("Осталось: " + bankAccount.getAmount());
+                    break;
+                }catch (LimitException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+}
